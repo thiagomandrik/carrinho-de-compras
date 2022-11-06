156c26a68c0b1c9f2857326193f19bd151415b1d
thiagomandrik <thiagomandrik@gmail.com>
2022-11-05 21:46:25 -0300

    estruturacao da section carrinho e funcoes de calculo do subtotal

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,11 @@
 //Definindo a lista de produtos
 var listaProdutos = [
-    {"produto":"Tijolo", "preco":1.31, "taxaIPI":0.08, "quantidade":0},
-    {"produto":"Cimento", "preco":33.90, "taxaIPI":0.08, "quantidade":0},
-    {"produto":"Telha", "preco":1.98, "taxaIPI":0.08, "quantidade":0}
+    {"produto":"Tijolo", "preco":1.31, "taxaIPI":0.08, "quantidade":0, "subtotal":0},
+    {"produto":"Cimento", "preco":33.90, "taxaIPI":0.04, "quantidade":0,"subtotal":0},
+    {"produto":"Telha", "preco":1.98, "taxaIPI":0.08, "quantidade":0, "subtotal":0}
 ];
+
+var quantidadeTotalProdutos = 0;
 
 //Funcao para formatar os precos em R$
 function formataPreco(preco){
@@ -20,21 +22,70 @@ function formataIPI(taxaIPI){
     ipiFormatado = ipiFormatado.replace(".",",");
 	ipiFormatado = ipiFormatado + "%";
 	return ipiFormatado;
+}    
+
+//Funcao para calcular o subtotal dos itens
+function subtotalItem(indiceProduto){
+    listaProdutos[indiceProduto].subtotal = (listaProdutos[indiceProduto].preco * listaProdutos[indiceProduto].quantidade) + (listaProdutos[indiceProduto].preco * listaProdutos[indiceProduto].quantidade) * listaProdutos[indiceProduto].taxaIPI;
 }
 
-
-function adicionaQuantidade(indiceProduto){
-    listaProdutos[indiceProduto].quantidade += 1;
+//Funcao para atualizar o carrinho de compras
+function atualizaCarrinho(indiceProduto){
     listaElementosQuantidadeDeProdutos[indiceProduto].innerHTML = `<b>${listaProdutos[indiceProduto].quantidade} und.</b>`;
+    subtotalItem(indiceProduto);
+    listaElementosTabelaPrecoProduto[indiceProduto].innerHTML = formataPreco(listaProdutos[indiceProduto].subtotal)
+    
+    quantidadeTotalProdutos = 0;
+    for(let contador = 0; contador < listaProdutos.length; contador++){
+        quantidadeTotalProdutos += listaProdutos[contador].quantidade;
+    }
+
+    if(quantidadeTotalProdutos>0){
+        document.getElementById("carrinho__produtosNoCarrinho--vazio").style.display = "none";
+        document.getElementById("carrinho__tabela").style.display = "inline-block";
+    }else{
+        document.getElementById("carrinho__produtosNoCarrinho--vazio").style.display = "inline-block";       
+        document.getElementById("carrinho__tabela").style.display = "none";       
+    }
 }
 
-function subtraiQuantidade(indiceProduto){
-    if(listaProdutos[indiceProduto].quantidade >0){
-        listaProdutos[indiceProduto].quantidade -= 1;
-        listaElementosQuantidadeDeProdutos[indiceProduto].innerHTML = `<b>${listaProdutos[indiceProduto].quantidade} und.</b>`;
+//Funcao para adicionar 1 item
+function adicionaQuantidade1(indiceProduto){
+    var quantidade = listaProdutos[indiceProduto].quantidade += 1;
+    atualizaCarrinho(indiceProduto, quantidade);
+}
+
+//Funcao para subtrair 1 item
+function subtraiQuantidade1(indiceProduto){
+    if(listaProdutos[indiceProduto].quantidade > 0){
+        var quantidade = listaProdutos[indiceProduto].quantidade -= 1;
+        atualizaCarrinho(indiceProduto, quantidade);
     } else{
         //pass
     }
+}
+
+//Funcao para adicionar 5 itens
+function adicionaQuantidade5(indiceProduto){
+    var quantidade = listaProdutos[indiceProduto].quantidade += 5;
+    atualizaCarrinho(indiceProduto, quantidade);
+}
+
+//Funcao para subtrair 5 itens
+function subtraiQuantidade5(indiceProduto){
+    if(listaProdutos[indiceProduto].quantidade >= 5){
+        var quantidade = listaProdutos[indiceProduto].quantidade -= 5;
+    } else{
+        listaProdutos[indiceProduto].quantidade = 0;
+    }
+    atualizaCarrinho(indiceProduto, quantidade);
+}
+
+//Funcao para limpar quantidade de item
+function limparQuantidadeItem(indiceProduto){
+    var quantidade = 0
+    listaProdutos[indiceProduto].quantidade = 0;
+    atualizaCarrinho(indiceProduto, quantidade);
 }
 
 //Listas dos elementos da section produtos
@@ -44,8 +95,11 @@ const listaElementosTaxaIpiDeProdutos = document.querySelectorAll('.produto__ipi
 const listaElementosQuantidadeDeProdutos = document.querySelectorAll('.produto__quantidade');
 
 //Lista dos botoes da section produtos 
-const listaBotoesAdiciona = document.querySelectorAll('.adiciona');
-const listaBotoesSubtrai = document.querySelectorAll('.subtrai');
+const listaBotoesAdiciona1 = document.querySelectorAll('.adiciona1');
+const listaBotoesSubtrai1 = document.querySelectorAll('.subtrai1');
+
+const listaBotoesAdiciona5 = document.querySelectorAll('.adiciona5');
+const listaBotoesSubtrai5 = document.querySelectorAll('.subtrai5');
 
 for(let contador = 0; contador < listaProdutos.length; contador++){
     //Definicao dos elementos iniciais da pagina
@@ -55,13 +109,35 @@ for(let contador = 0; contador < listaProdutos.length; contador++){
     listaElementosQuantidadeDeProdutos[contador].innerHTML = `<b>${listaProdutos[contador].quantidade} und.</b>`;
     
     //Definicao das funcoes dos botoes adiciona e subtrai
-    listaBotoesAdiciona[contador].onclick = function(){
-        adicionaQuantidade(contador);
+    listaBotoesAdiciona1[contador].onclick = function(){
+        adicionaQuantidade1(contador);
     };
 
-    listaBotoesSubtrai[contador].onclick = function(){
-        subtraiQuantidade(contador);
+    listaBotoesSubtrai1[contador].onclick = function(){
+        subtraiQuantidade1(contador);
     };
-    
+
+    listaBotoesAdiciona5[contador].onclick = function(){
+        adicionaQuantidade5(contador);
+    };
+
+    listaBotoesSubtrai5[contador].onclick = function(){
+        subtraiQuantidade5(contador);
+    };
+    document.getElementById("carrinho__tabelaLinhas").innerHTML +=
+    `<tr>
+        <td class="carrinho__tabelaNomeProduto">${listaProdutos[contador].produto}</td>
+        <td class="carrinho__tabelaPrecoProduto">R$ 0,00</td>
+        <td>
+            <button class="carrinho__botaoLimparQuantidade">Limpar</button>
+        </td>`;
 }
 
+const listaElementosTabelaPrecoProduto = document.querySelectorAll(".carrinho__tabelaPrecoProduto");
+const listaBotoesLimparQuantidadeItens = document.querySelectorAll(".carrinho__botaoLimparQuantidade");
+
+for(let contador = 0; contador < listaBotoesLimparQuantidadeItens.length; contador++){
+    listaBotoesLimparQuantidadeItens[contador].onclick = function(){
+        limparQuantidadeItem(contador);
+    }
+}
